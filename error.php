fd62ba401139755d88f508b5aa179e232eabd1ac
Nikita Balashov <nikita.89534588306@gmail.com>
2022-09-14 16:25:49 +0300

    create block head

--- error.php
+++ error.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Ошибка 404</title>
-  <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <link rel="icon" href="/img/favicon.ico">
-  <link rel="stylesheet" href="/css/main.css">
+<?php
+    $website_title = 'Ошибка 404';
+    require 'blocks/head.php';
+  ?>
 </head>
 <body>
   <?php require 'blocks/header.php'; ?>
